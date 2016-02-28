b504e4958830cb60b11057c082ded98c0b72b24f
kot <stas@jresearch.org>
2016-02-28 13:35:28 +0100

    Reformat sources

--- gwt2go/src/main/java/com/gwt2go/dev/linker/AppCacheManifestLinker.java
+++ gwt2go/src/main/java/com/gwt2go/dev/linker/AppCacheManifestLinker.java
@@ -4,19 +4,19 @@ import com.google.gwt.core.ext.linker.Shardable;
 
 /**
  * Application Linker creates the cacheable manifest file
- * 
+ *
  * @author L.Pelov
  */
 @Shardable
 public class AppCacheManifestLinker extends ManifestLinker {
 
-	@Override
-	protected String[] otherCachedFiles() {
-		return new String[] { "/2Gwt2go.html", "/2Gwt2go.css" };
-	}
+    @Override
+    protected String[] otherCachedFiles() {
+        return new String[] { "/2Gwt2go.html", "/2Gwt2go.css" }; //$NON-NLS-1$ //$NON-NLS-2$
+    }
 
-	@Override
-	protected String appCacheManifestTemplate() {
-		return "myappcache.manifest";
-	}
+    @Override
+    protected String appCacheManifestTemplate() {
+        return "myappcache.manifest"; //$NON-NLS-1$
+    }
 }
